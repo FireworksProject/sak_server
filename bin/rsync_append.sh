8eb6db7e8085a212b1f27e092e573432189d767e
Kristoffer Walker <kris@kixx.name>
2012-01-18 08:57:20 -0500

    shared projects push and pull
     On branch master
    
    	new file:   bin/install_bin
    	new file:   bin/pull_shared_projects
    	new file:   bin/push_shared_projects
    	modified:   bin/rsync_append.sh
    	deleted:    bin/rsync_merge.sh
    	modified:   bin/setup_fwpusers

--- bin/rsync_append.sh
+++ bin/rsync_append.sh
@@ -7,7 +7,7 @@ source "$LIBDIR/utils.sh"
 if [ -z $1 ]; then
     echo "Mirror files from a src to a destination without deleting"
     echo
-    echo "  <src> <dest> <logfile> [<excludefile>]"
+    echo "  <src> <dest> <logfile>"
     echo
     echo "! <src> and <dest> directory names to *not* need to have a trailing /"
     exit 0
@@ -17,9 +17,12 @@ main () {
     local src="$1"
     local dest="$2"
     local logfile="$3"
-    local excludefile="$4"
 
-    local options="\
+    if [ -z "$logfile" ]; then
+        fail "$0: logfile is a required argument"
+    fi
+
+    rsync \
         --recursive \
         --links \
         --perms \
@@ -27,18 +30,9 @@ main () {
         --owner \
         --progress \
         --human-readable \
-        "
-    if [ -z "$logfile" ]; then
-        fail "$0: logfile is a required argument"
-    fi
-
-    options="$options --log-file=$logfile"
-
-    if [ -f "$excludefile" ]; then
-        options="$options --exclude-from=$excludefile"
-    fi
-
-    rsync $options "$src/" "$dest/" || fail "$0: rsync failed"
+        --log-file="$logfile" \
+        -e 'ssh -p 2575' \
+        "$src/" "$dest/" || fail "$0: rsync failed"
 }
 
 main "$@"
